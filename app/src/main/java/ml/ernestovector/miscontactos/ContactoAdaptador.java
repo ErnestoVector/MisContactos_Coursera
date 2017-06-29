0ad679bd0af8fd80e3a8620e000564ec1e5f8149
ErnestoVector <egm.vector@gmail.com>
2017-06-29 11:28:13 -0500

    RecyclerView terminado en modo Grid

--- app/src/main/java/ml/ernestovector/miscontactos/ContactoAdaptador.java
+++ app/src/main/java/ml/ernestovector/miscontactos/ContactoAdaptador.java
@@ -2,11 +2,49 @@ package ml.ernestovector.miscontactos;
 
 import android.media.Image;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-public class ContactoAdaptador {
+import java.util.ArrayList;
+
+public class ContactoAdaptador extends RecyclerView.Adapter<ContactoAdaptador.ContactoViewHolder>{
+
+    //Lista contactos
+    ArrayList<Contacto> contactos;
+
+    //Método constructor
+    public ContactoAdaptador(ArrayList<Contacto> contactos){
+        this.contactos = contactos;
+    }
+
+
+    //Le da vida al layout CardView
+    //Infla el layout y lo pasa al viewholder para que obtenga los views
+    @Override
+    public ContactoViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        //Asociamos el cardview_contacto con el main activity
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.cardview_contacto, parent, false);
+
+        return new ContactoViewHolder(v);
+    }
+
+    //Asocia cada elemento de la lista a cada view
+    @Override
+    public void onBindViewHolder(ContactoViewHolder contactoViewHolder, int position) {
+        Contacto contacto = contactos.get(position);
+        contactoViewHolder.imgFoto.setImageResource(contacto.getFoto());
+        contactoViewHolder.tvNombreCv.setText(contacto.getNombre());
+        contactoViewHolder.tvTelefonoCV.setText(contacto.getTelefono());
+    }
+
+    //Cantidad de elementos que contiene mi lista
+    @Override
+    public int getItemCount() {
+        return contactos.size();
+    }
 
     public static class ContactoViewHolder extends RecyclerView.ViewHolder{
 
--- app/src/main/java/ml/ernestovector/miscontactos/MainActivity.java
+++ app/src/main/java/ml/ernestovector/miscontactos/MainActivity.java
@@ -3,6 +3,10 @@ package ml.ernestovector.miscontactos;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -13,26 +17,36 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
 
     ArrayList<Contacto> contactos;
+    private RecyclerView listaContactos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        contactos = new ArrayList<Contacto>();
+        //Toolbar miActionBar = (Toolbar) findViewById(R.id.miActionBar);
+        //setSupportActionBar(miActionBar);
 
-        contactos.add(new Contacto(R.drawable.six_flags_s_perman,   "Ernesto Gálvez",  "55 3332 2391", "egm.vector@gmail.com"));
-        contactos.add(new Contacto(R.drawable.coleos1,              "Anahí Salgado",   "55 7777 9999", "anahi@gmail.com"));
-        contactos.add(new Contacto(R.drawable.snapdragon,           "Lori Martínez",   "55 2256 3487", "lori-carolina@hotmail.com"));
-        contactos.add(new Contacto(R.drawable.fanny,                "Estefani Monroy", "55 1845 7222", "fanny.monroy2204@gmail.com"));
-        contactos.add(new Contacto(R.drawable.dusty_miller,         "Carlos Guerrero", "55 2300 2300", "carlitos@outlook.com"));
+        listaContactos = (RecyclerView) findViewById(R.id.rvContactos);
 
+        GridLayoutManager glm = new GridLayoutManager(this, 2);
+        listaContactos.setLayoutManager(glm);
+
+        //Muestra los objetos en una lista scrolleable hacia abajo
+        //LinearLayoutManager llm = new LinearLayoutManager(this);
+        //llm.setOrientation(LinearLayoutManager.VERTICAL);
+        //listaContactos.setLayoutManager(llm);
+
+        inicializarListaContactos();
+        inicializarAdaptador();
+
+        /*
         ArrayList<String> nombresContacto = new ArrayList<>();
         for (Contacto contacto: contactos) {
             nombresContacto.add(contacto.getNombre());
         }
 
-        /*
+
         ListView lstContactos = (ListView) findViewById(R.id.lstContactos);
         lstContactos.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, nombresContacto));
 
@@ -48,5 +62,21 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });*/
+    }
+
+    public void inicializarAdaptador(){
+        ContactoAdaptador adaptador = new ContactoAdaptador(contactos);
+        listaContactos.setAdapter(adaptador);
+    }
+
+    public void inicializarListaContactos(){
+        contactos = new ArrayList<Contacto>();
+
+        contactos.add(new Contacto(R.drawable.six_flags_s_perman,   "Ernesto Gálvez",  "55 3332 2391", "egm.vector@gmail.com"));
+        contactos.add(new Contacto(R.drawable.coleos1,              "Anahí Salgado",   "55 7777 9999", "anahi@gmail.com"));
+        contactos.add(new Contacto(R.drawable.snapdragon,           "Lori Martínez",   "55 2256 3487", "lori-carolina@hotmail.com"));
+        contactos.add(new Contacto(R.drawable.fanny,                "Estefani Monroy", "55 1845 7222", "fanny.monroy2204@gmail.com"));
+        contactos.add(new Contacto(R.drawable.dusty_miller,         "Carlos Guerrero", "55 2300 2300", "carlitos@outlook.com"));
+
     }
 }
